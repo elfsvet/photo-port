0253660244db79ac5fbb2b7370bdbb64bc5c3d4f
elfsvet <stepanmatysik@gmail.com>
2022-07-16 12:01:52 -0400

    added first step in about

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import logo from './logo.svg';
+
+const About = () => {
+  return (
+    // React.createElement('div',{},[
+      // React.createElement('h1',{}, 'Stepan'),
+      // React.createElement('p',{}, 'breed: Human')
+    // ])
+    <div>
+      <h1>Stepan</h1>
+      <p>Human</p>
+    </div>
+  );
+}
+
+export default About;
