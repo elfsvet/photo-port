a1b4dfe0f55f7ec0e7b234b777b4033c2b9f6020
elfsvet <stepanmatysik@gmail.com>
2022-07-17 22:42:58 -0400

    finished with tests for nav and gallery and photo list

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
+import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from '../../assets/small/commercial/0.jpg';
 
-const Gallery = (props) => {
-  const currentCategory = {
-    name: "commercial",
-    description:
-      "Photos of grocery stores, food trucks, and other commercial projects",
-  };
+const Gallery = ({ currentCategory }) => {
+  const { name, description } = currentCategory;
 
   return (
     // React.createElement('tag',{},[
@@ -15,15 +11,9 @@ const Gallery = (props) => {
     // React.createElement('tag',{}, 'text of tag')
     // ])
     <section className="my-5">
-      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-      <p>{currentCategory.description}</p>
-      <div>
-        <img
-          src={photo}
-          alt="Commercial Example"
-          className='img-thumbnail mx-1'
-        />
-      </div>
+      <h1 data-testid='h1tag'>{capitalizeFirstLetter(name)}</h1>
+      <p>{description}</p>
+      <PhotoList category={currentCategory.name} />
     </section>
 
   );
