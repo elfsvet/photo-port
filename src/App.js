22179ca67e459cdbeb50e243687354c1729a5680
elfsvet <stepanmatysik@gmail.com>
2022-07-17 20:13:55 -0400

    added harcoded gallery

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import React from 'react';
 import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 // JSX
 function App() {
   return (
     <div>
-      <Nav/>
+      <Nav />
       <main>
-
-        <About/>
+        <Gallery />
+        <About />
       </main>
     </div>
   );
